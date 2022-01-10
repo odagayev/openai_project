f29466575ffae00d347e4479d338609bdb665c35
Sasha Dagayev <odagayev@gmail.com>
2022-01-10 12:31:20 -0500

    working default prototype

--- open_ai_project.py
+++ open_ai_project.py
@@ -1,11 +1,19 @@
 import openai as ai
 import os as os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
 ai.api_key = os.getenv('OPENAI_API_KEY')
 
-response = ai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
+st.sidebar.title('Configurations')
+st.header('This is an Explainer App')
+st.subheader('This app uses OpenAI API to summarize text entered into the text box into a summary')
+text_input = st.text_input('Enter your text here')
+clicked_button  = st.button('click me') 
+print(text_input)
 
-print(response)
+if clicked_button:
+    response = ai.Completion.create(engine="davinci", prompt=text_input, max_tokens=5)
+    st.write(response.choices[0].text)
