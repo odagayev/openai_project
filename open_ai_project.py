fcef2dce58a41e553d11e81cc605e8558a721d57
Sasha Dagayev <odagayev@gmail.com>
2022-01-10 12:08:12 -0500

    env files

--- open_ai_project.py
+++ open_ai_project.py
@@ -1,5 +1,11 @@
 import openai as ai
+import os as os
+from dotenv import load_dotenv
 
-ai.api_key = 'YOUR_API_KEY'
+load_dotenv()
+
+ai.api_key = os.getenv('OPENAI_API_KEY')
 
 response = ai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
+
+print(response)
